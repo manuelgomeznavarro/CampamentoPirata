e6371713982d359cae74ad81556a2d62bab6264c
Dani <danisimmon7@gmail.com>
2025-02-02 13:03:54 +0100

    Creación migración de tablas

---
+++ backend/database/migrations/2025_02_02_113634_create_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('administrator_id')->constrained('admins')->onDelete('set null');
+            $table->foreignId('monitor_id')->constrained('monitors')->onDelete('set null');
+            $table->foreignId('timeline_id')->constrained('timelines')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
---
+++ backend/database/migrations/2025_02_02_113757_create_incidents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subject');
+            $table->date('date');
+            $table->string('status');
+            $table->foreignId('tutor_id')->constrained('tutors')->onDelete('set null');
+            $table->foreignId('admin_id')->constrained('admins')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incidents');
+    }
+};
