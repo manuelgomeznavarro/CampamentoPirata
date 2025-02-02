b36cbbd563a9a9e7a293c96b92eaf672db9e8ec8
gonzalopozo <gonzalopozo2005@gmail.com>
2025-02-02 13:06:11 +0100

    Creación migraciones tablas tutors, timelines y prices para MySQL

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $databaseName = Config::get('database.connections.mysql.database');
+
+        try {
+            DB::statement("CREATE DATABASE IF NOT EXISTS $databaseName");
+            Config::set('database.connections.mysql.database', $databaseName);
+            DB::reconnect();
+
+            Artisan::call('migrate');
+        } catch (\Exception $e) {
+            die("Error creating database: " . $e->getMessage());
+        }        
     }
 }
+
