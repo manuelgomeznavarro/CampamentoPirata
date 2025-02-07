4afc4955da200880571c565af26452b1f3de0e4b
Dani <danisimmon7@gmail.com>
2025-02-07 19:44:52 +0100

    Ventanas gestión de monitores del administrador

---
+++ frontend/admin/monitores.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btnCrearMonitores = document.getElementById("btnCrearMonitores");
+    const btnGrupos = document.getElementById("btnGrupos");
+    const btnListaMonitores = document.getElementById("btnListaMonitores");
+    const formCrearMonitores = document.getElementById("form-crear-monitor");
+    const grupos = document.getElementById("grupos");
+    const listaMonitores = document.getElementById("lista-monitores");
+
+    btnCrearMonitores.addEventListener('click', function () {
+        if (formCrearMonitores.style.display === "none") {
+            console.log("hola");
+            formCrearMonitores.style.display = "flex";
+            grupos.style.display = "none";
+            listaMonitores.style.display = "none";
+        } else {
+            formCrearMonitores.style.display = "none";
+        }
+    });
+
+    btnGrupos.addEventListener('click', function () {
+        if (grupos.style.display === "none") {
+            grupos.style.display = "block";
+            formCrearMonitores.style.display = "none";
+            listaMonitores.style.display = "none";
+        } else {
+            grupos.style.display = "none";
+        }
+    });
+
+    btnListaMonitores.addEventListener('click', function () {
+        if (listaMonitores.style.display === "none") {
+            listaMonitores.style.display = "block";
+            formCrearMonitores.style.display = "none";
+            grupos.style.display = "none";
+        } else {
+            listaMonitores.style.display = "none";
+        }
+    });
+});
