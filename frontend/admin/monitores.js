f0fa740e04e8a83cc3d960687d64b1bc82cbd771
Dani <danisimmon7@gmail.com>
2025-02-08 18:52:53 +0100

    Implentación del estado activo en los botones del administrador

--- frontend/admin/monitores.js
+++ frontend/admin/monitores.js
@@ -37,6 +37,22 @@ document.addEventListener('DOMContentLoaded', function () {
     //Agrego la clase 'activo' al botón Dashboard
     defaultButton.classList.add('activo');
 
+    document.querySelectorAll('.button-options').forEach(function (boton) {
+        // Agrego un evento 'click' a cada botón
+        boton.addEventListener('click', function () {
+            
+            if (this.classList.contains('activo')) {
+                boton.classList.remove('activo');
+            } else {
+                // Elimino la clase 'activo' de todos los botones
+            // Esto es para que solo un botón tenga la clase 'activo' a la vez
+            document.querySelectorAll('.button-options').forEach(b => b.classList.remove('activo'));
+            // Agrego la clase 'activo' al botón al que se le hizo click
+            this.classList.add('activo');
+            }
+        });
+    });
+
     // document.querySelectorAll('.menu-item').forEach(function (boton) {
     //     // Agrego un evento 'click' a cada botón
     //     boton.addEventListener('click', function () {
