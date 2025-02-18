de78fd13beb174181982ff45a237d413e19206a3
Manuel Gomez <mgn14@hotmail.es>
2025-02-18 18:35:43 +0100

    Cambios sección tarifa

---
+++ frontend/tutor/tarifas/tarifas.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+//Fetch para obtener los datos de las tarifas de la BBDD
+fetch('http://127.0.0.1:8000/api/prices')
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`Error `);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    data.forEach((item, index) => {
+        console.log(`ID: ${item.id}, Nombre Tarifa: ${item.name}, Precio: ${item.price}`);
+        console.log(index);
+        const tarifaCards = document.querySelectorAll('.tarifa-card');
+        if (index < tarifaCards.length) {
+            const tarifaCard = tarifaCards[index];
+            const titleElement = tarifaCard.querySelector('.plan-title span');
+            const priceElement = tarifaCard.querySelector('.plan-price span');
+            // const buttonsave = tarifaCard.querySelector('button');
+            titleElement.textContent = item.name;
+            priceElement.textContent = item.price;
+            // buttonsave.value = item.id;
+        }
+    })
+})
+.catch(error => {
+    console.error('Error al obtener los datos:', error);
+});
+})
