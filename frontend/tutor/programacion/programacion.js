dc3ca2b93326f8f1aa1ea516706f1b073aa09dbd
Dani <danisimmon7@gmail.com>
2025-02-26 20:10:17 +0100

    Se han añadido el nombre de las actividades que nos pasaron las de infantil, y se han creado las tarjetas de dichas actividades de forma dinámica

---
+++ frontend/tutor/programacion/programacion.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //Fetch para obtener los datos de las tarifas de la BBDD
+    fetch('http://127.0.0.1:8000/api/activities')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error `);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const cardsContainer = document.querySelector('.cards-container');
+            data.forEach((item, index) => {
+                console.log(`ID: ${item.id}, Nombre Actividad: ${item.name}, Descripción: ${item.description}`);
+                console.log(index);
+                // Crear una nueva tarjeta
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                // Crear el elemento figure y la imagen
+                const figure = document.createElement('figure');
+                const img = document.createElement('img');
+                img.src = "https://placehold.co/408x126";
+                img.alt = "";
+
+                // Agregar la imagen al figure
+                figure.appendChild(img);
+
+                // Crear el contenedor de texto
+                const textContainer = document.createElement('div');
+
+                // Crear el título de la actividad
+                const title = document.createElement('h6');
+                title.textContent = item.name;
+
+                // Crear la descripción de la actividad
+                const description = document.createElement('p');
+                description.textContent = item.description;
+
+                // Agregar el título y la descripción al contenedor de texto
+                textContainer.appendChild(title);
+                textContainer.appendChild(description);
+
+                // Agregar el figure y el contenedor de texto a la tarjeta
+                card.appendChild(figure);
+                card.appendChild(textContainer);
+
+                // Agregar la tarjeta al contenedor de tarjetas
+                cardsContainer.appendChild(card);
+            });
+            
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+        });
+
+
+});
