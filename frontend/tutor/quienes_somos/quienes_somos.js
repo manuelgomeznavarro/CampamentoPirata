994f7e56a54b301265c69a86f6a3404ec9a37450
Manuel Gomez <mgn14@hotmail.es>
2025-02-27 20:01:27 +0100

    Realizados los botones de perfil y log-out del index

--- frontend/tutor/quienes_somos/quienes_somos.js
+++ frontend/tutor/quienes_somos/quienes_somos.js
@@ -14,6 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
                     const tutorProfilePicContainer = document.createElement('figure');
                     const tutorProfilePic = document.createElement('img');
 
+                    // tutorProfilePic.src = 'https://campamento-tesoro-perdido-image-hosting.fra1.cdn.digitaloceanspaces.com/icons/default-profile.png';
+                    tutorProfilePicContainer.addEventListener('click', () => {
+                        location.assign('../../tutor/perfil_tutor/perfil_tutor.html');
+                    });
+                    const btnSalir = document.createElement('button');
+                    const imgSalir = document.createElement('img');
+                    imgSalir.src = 'https://campamento-tesoro-perdido-image-hosting.fra1.cdn.digitaloceanspaces.com/icons/logout-icon.png';
+                    btnSalir.appendChild(imgSalir);
+
+                    btnSalir.addEventListener('click', () => {
+                        localStorage.removeItem('role');
+                        localStorage.removeItem('role_id');
+                        location.reload();
+                    });
                     fetch(`http://127.0.0.1:8000/api/get_user_pic`, {
                         method: 'POST', //Método para enviar los datos al servidor
                         headers: {
@@ -44,7 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     headerBtnsContainer.innerHTML = "";
                     headerBtnsContainer.appendChild(reservationBtn);
                     headerBtnsContainer.appendChild(tutorProfilePicContainer);
-
+                    headerBtnsContainer.appendChild(btnSalir);
+                    tutorProfilePicContainer.appendChild(tutorProfilePic);
                     break;
 
                 case 'monitor':
